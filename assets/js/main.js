03976de08a10c051c7f896610f65d858c2be45bd
Dan97O <dan97-oddo@live.it>
2023-03-16 14:27:08 +0100

    add palindromic word

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,41 @@ Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
 
 
+//Palidromo
+//chiedere all'utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+
+//chiedere all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+const reverseWord = wordReverse(userWord);
+
+//Creare una funzione per capire se la parola inserita è palindroma
+function wordReverse(word) {
+  const reverse = word.split('').reverse().join('');
+
+  return reverse;
+}
+
+if(userWord == reverseWord){
+  console.log('La parola è palindroma');
+} else {
+  console.log('La parola non è palindroma');
+}
+
+
+
+
+
+
+//Pari e Dispari
+//scengo pari o dispari e inserisco un numero da 1 a 5
+//generiamo un numero random (1/5) per il pc (usando una funzione)
+//sommiamo i due numeri
+//stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//dichiariamo chi ha vinto
+
+
+
+
+
 
